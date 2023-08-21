4767b268a61bb240380d6129797e59389621ee47
movptr <movptr06@outlook.kr>
2023-08-21 16:00:44 +0900

    Feat: Load rule files

--- src/config/config.py
+++ src/config/config.py
@@ -1,33 +1,64 @@
 from typing import *
 from ipaddress import IPv4Address, IPv6Address, IPv4Network, IPv6Network
 
+import os
 import yaml
 
+from ruleset.ruleset import RuleSet
+from ruleset.action import Action
+from ruleset.rule import Rule
+
 class Config:
-    def __init__(self, config_yaml):
-        config = yaml.safe_load(config_yaml)
+    def _load_rule(filename):
+        filenames = []
+        if os.path.isdir(filename):
+            for path, subdirs, files in os.walk(filename):
+                for fn in files:
+                    filenames.append(path + "/" + fn)
+        else:
+            filenames += [filename]
 
-        self.forward = []
+        files = []
+        for fn in filenames:
+            with open(fn, "rt") as f:
+                files.append(Rule(f.read()))
 
-        for ip in config["x-forwarded-for"]:
-            try:
-                self.forward.append(IPv4Network(ip))
-            except:
-                self.forward.append(IPv6Network(ip))
+        return files
 
-        self.allowlist = [
-            (
-                IPv4Network(allow["ipv4Network"])
-                if "ipv4Network" in allow else None,
+    def __init__(self, filename):
+        with open(filename, "rt") as f:
+            config = yaml.safe_load(f)
 
-                IPv6Network(allow["ipv6Network"])
-                if "ipv6Network" in allow else None,
+        self.forward = [IPv4Network("127.0.0.1"), IPv6Network("::1")]
+        if "x-forwarded-for" in config:
+            for ip in config["x-forwarded-for"]:
+                try:
+                    self.forward.append(IPv4Network(ip))
+                except:
+                    self.forward.append(IPv6Network(ip))
 
-                allow["userAgent"]
-                if "userAgent" in allow else None
-            )
-            for allow in config["allow"]
-        ]
+        if "allow" in config:
+            self.allowlist = [
+                (
+                    IPv4Network(allow["ipv4Network"])
+                    if "ipv4Network" in allow else None,
+
+                    IPv6Network(allow["ipv6Network"])
+                    if "ipv6Network" in allow else None,
+
+                    allow["userAgent"]
+                    if "userAgent" in allow else None
+                )
+                for allow in config["allow"]
+            ]
+
+        self.rules = []
+        for rule_config in config["rules"]:
+            rule_list = Config._load_rule(rule_config["file"])
+            for rule in rule_list:
+                if "action" in rule_config.keys():
+                    rule.action = Action.serialize(rule_config["action"])
+            self.rules += rule_list
 
     def allow(self, ip, ipv4, ipv6, user_agent):
         for net in self.forward:
